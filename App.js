c6a1e83fe62b3bf9e10d3152eae3807b6c84a76e
Oguzhan Atalay <oguzhnatly@gmail.com>
2020-09-20 16:00:50 +0300

    Setting Up Redux

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React, { Component } from 'react';
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Warning:', 'Setting', 'Failed', '']);
+import Main from './components/MainComponent';
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <Main />
+    );
+  }
 }
+// javascript array operations in react native
