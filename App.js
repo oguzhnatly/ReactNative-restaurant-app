2a557f91f1d1adb88dac6970d1183bc254c1680b
Oguzhan Atalay <oguzhnatly@gmail.com>
2020-09-27 12:43:39 +0300

    Persist Redux Store

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+import { PersistGate } from 'redux-persist/es/integration/react'
+import { Loading } from './components/LoadingComponent';
 
 import { AppRegistry } from 'react-native';
 AppRegistry.registerComponent('main',() => App);
@@ -9,13 +11,17 @@ AppRegistry.registerComponent('main',() => App);
 import { YellowBox } from 'react-native';
 YellowBox.ignoreWarnings(['Setting DrawerLayoutAndroid', 'Warning: componentWillReceiveProps', '']);
 
-const store = ConfigureStore();
+const { persistor, store } = ConfigureStore();
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Main />
+        <PersistGate 
+          loading={<Loading />}
+          persistor={persistor}>
+          <Main />
+        </PersistGate>
       </Provider>
     );
   }
--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -24,7 +24,7 @@ function RenderDish(props) {
     const dish = props.dish;
     handleViewRef = ref => this.view = ref;
     const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
-        if ( dy < -200 )
+        if ( dx < -200 )
             return true;
         else
             return false;
@@ -38,7 +38,7 @@ function RenderDish(props) {
         },
         onPanResponderEnd: (e, gestureState) => {
             console.log("pan responder end", gestureState);
-            if (recognizeDrag(gestureState))
+            if (!recognizeDrag(gestureState))
                 Alert.alert(
                     'Add Favorite',
                     'Are you sure you wish to add ' + dish.name + ' to favorite?',
@@ -48,7 +48,6 @@ function RenderDish(props) {
                     ],
                     { cancelable: false }
                 );
-
             return true;
         }
     })
